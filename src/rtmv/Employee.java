fe8f5d2e13e7b525f33e38f2233275dee7f7b291
Jayson Rael Ramos <lothlorien654@gmail.com>
2020-02-11 01:42:28 +0800

    Create Employee Class

---
+++ src/rtmv/Employee.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rtmv;
+
+/**
+ *
+ * @author MIC-P ICT
+ */
+public class Employee extends Person{
+    
+}
